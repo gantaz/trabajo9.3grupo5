966e3f2cc3b7c568d28cb2cf95fec497aa289b3c
Gabriel Núñez <gabrielnz23.gne@gmail.com>
2023-08-24 19:12:16 -0300

    Se acrego que se vean los items al recargar iniciar la pagina

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,5 +1,11 @@
-let boton = document.getElementById("agregar");
+const boton = document.getElementById("agregar");
 let listaItems = JSON.parse(localStorage.getItem('listaItems')) || [];
+let arrayItems = document.getElementById("contenedor");
+let limpiar = document.getElementById("limpiar")
+
+document.addEventListener("DOMContentLoaded", function () {
+    arrayItems.innerHTML = JSON.parse(localStorage.getItem('listaItems')).join('<br>');
+});
 
 boton.addEventListener('click', () => {
     let nuevoItem = document.getElementById("item").value;
@@ -7,5 +13,14 @@ boton.addEventListener('click', () => {
     listaItems.push(nuevoItem);
 
     localStorage.setItem('listaItems', JSON.stringify(listaItems));
+
+    arrayItems.innerHTML = JSON.parse(localStorage.getItem('listaItems')).join('<br>');
+});
+
+limpiar.addEventListener('click', () => {
+    listaItems = [];
+    localStorage.removeItem('listaItems');
+    arrayItems.innerHTML = "";
+
 });
 
